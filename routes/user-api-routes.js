225a9184e353556fa842ce0fa6ea94709ff63ff0
tearshred <vmiskeljin@gmail.com>
2019-03-31 18:34:42 -0700

    updated stuff

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,35 +1,48 @@
 var db = require("../models");
-
-module.exports = function(app) {
-  // Get all users
-  app.get("/api/user", function(req, res) {
-    db.User.findAll({}).then(function(dbUser) {
-      res.json(dbUser);
-    });
+​
+module.exports = function (app) {
+ // Get all users
+ app.get("/api/users", function (req, res) {
+  db.User.findAll({}).then(function (dbUser) {
+   res.json(dbUser);
   });
-
-  // Get a specific user
-  app.get("/api/user/:id", function(req, res){
-    db.User.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbUser){
-      res.json(dbUser);
-    });
+ });
+​
+ // Get a specific user
+ app.get("/api/user/:id", function (req, res) {
+  db.User.findOne({
+   where: {
+    id: req.params.id
+   }
+  }).then(function (dbUser) {
+   res.json(dbUser);
   });
-
-  // Create a new user
-  app.post("/api/users", function(req, res) {
-    db.User.create(req.body).then(function(dbUser) {
-      res.json(dbUser);
-    });
+ });
+​
+ // Create a new user
+ app.post("/api/users", function (req, res) {
+  db.User.create({
+   firstName: req.body.firstNameInput,
+   lastName: req.body.lastNameInput,
+   addressLine1: req.body.addressLine1Input,
+   addressLine2: req.body.addressLine2Input,
+   city: req.body.cityInput,
+   state: req.body.stateInput,
+   zip: req.body.zipCodeInput,
+   SSN: req.body.SNNinput,
+   DOB: req.body.DOBinput,
+   email: req.body.emailInput,
+   username: req.body.usernameInput
+  }).then(function (dbUser) {
+   res.json(dbUser);
+   console.log("posted");
   });
-
-  // Delete an user by id
-  app.delete("/api/user/:id", function(req, res) {
-    db.User.destroy({ where: { id: req.params.id } }).then(function(dbUser) {
-      res.json(dbUser);
-    });
+ });
+​
+ // Delete an user by id
+ app.delete("/api/user/:id", function (req, res) {
+  db.User.destroy({ where: { id: req.params.id } }).then(function (dbUser) {
+   res.json(dbUser);
   });
+ });
 };
